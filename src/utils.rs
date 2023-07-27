0adb4f8f7ce1c7f7bbf25d11985da4cc1d17e5e9
oether <oether@oether.com>
2023-07-27 19:06:42 +0200

    minor utils.rs changes

--- src/utils.rs
+++ src/utils.rs
@@ -8,9 +8,15 @@ pub fn sleep(seconds: f64) {
     tsleep(Duration::from_secs_f64(seconds));
 }
 
-pub fn pathbuf_to_string(pathbuf: &PathBuf, max_length: usize, error_str: &str) -> String {
+pub fn pathbuf_to_string(pathbuf: &PathBuf, desired_length: usize, error_str: &str) -> String {
     let filestem: &str = pathbuf.file_stem().unwrap_or(error_str.as_ref()).to_str().unwrap_or(error_str);
-    if filestem.len() > max_length + 2 {
-        String::from_iter([&filestem[0..max_length-2], ".."])
-    } else {String::from(filestem)}
+    if filestem.len() > desired_length {
+        String::from_iter([&filestem[0..desired_length-2], ".."])
+    } else {
+        let mut string: String = String::from(filestem);
+        while string.len() < desired_length {
+            string.push(' ');
+        }
+        string
+    }
 }
